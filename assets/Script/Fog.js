03707e8153e9a39c541362c97def0a67e1a637c2
dibashi <dibashi1989@163.com>
2018-12-21 15:57:46 +0800

    宝箱逻辑完善

--- assets/Script/Fog.js
+++ assets/Script/Fog.js
@@ -9,9 +9,9 @@ cc.Class({
             default: null,
             type: cc.Node
         },
-        box:{
+        box: {
             default: null,
-            type:cc.Node
+            type: cc.Node
         },
 
         box_0_spr: {
@@ -28,23 +28,76 @@ cc.Class({
             type: cc.SpriteFrame
         },
 
-        reLockLabel:{
-            default:null,
-            type:cc.Label
-        }
+        reLockLabel: {
+            default: null,
+            type: cc.Label
+        },
+
+        thingPrefab: {
+            default: null,
+            type: cc.Prefab,
+        },
+
+        dragonPrefab: {
+            default: null,
+            type: cc.Prefab,
+        },
+
+       
         // defaults, set visually when attaching this script to the Canvas
 
     },
 
-    boxClick:function() {
+    boxClick: function () {
         console.log('宝箱被点击了！');
+
+        var treasureData = cc.dataMgr.randomTreasure();
+
+        switch (treasureData.category) {
+            case "coin":
+
+                break;
+
+            case "flower":
+            this.fog_GenerateThing(2, treasureData.level);
+                break;
+
+            case "dragon":
+
+                break;
+
+            case "heart":
+               this.fog_GenerateThing(1, treasureData.level);
+                
+                break;
+
+            default:
+                break;
+        }
+
     },
 
-    fogClick:function() {
+    fog_GenerateThing: function (thingType, thingLevel) {
+        var newThing = cc.instantiate(this.thingPrefab);
+        newThing.position = this.node.position;
+        var thingJS = newThing.getChildByName('selectedNode').getComponent('Thing');
+        thingJS.setTypeAndLevel_forNewThing(thingType, thingLevel);
+        
+
+        cc.find("Canvas/gameLayer/thingsNode").addChild(newThing);
+       
+    
+        thingJS.changeInTile(this.tile, thingLevel, thingType);
+    
+        this.node.destroy();
+    },
+
+
+    fogClick: function () {
         console.log('fog被点击了！');
 
         var heartCount = cc.dataMgr.getHeartCount();
-        if(heartCount>this.fogAmount) {
+        if (heartCount > this.fogAmount) {
             //心够，将来可能是弹窗
             //解锁雾 可以在动画回调里设置
             cc.dataMgr.addHeartCount(-this.fogAmount);
@@ -56,25 +109,27 @@ cc.Class({
         }
     },
 
-    settingFog:function(fogState,fogAmount) {
-       
+    settingFog: function (fogState, fogAmount,tile) {
+
         this.fogAmount = fogAmount;
         this.settingState(fogState);
+
+        this.tile = tile;
     },
 
-    settingState:function(fogState) {
+    settingState: function (fogState) {
         this.fogState = fogState;
-        if(this.fogState == 0) {
+        if (this.fogState == 0) {
             this.reLockLabel.string = this.fogAmount + "精华解锁";
             this.box.active = false;
-        } else if(this.fogState == 1) {
+        } else if (this.fogState == 1) {
             this.reLockLabel.node.active = false;
-            this.fog.active =false;
+            this.fog.active = false;
             this.box.active = true;
         } else {
-            console.log("未知 fogState " +fogState);
+            console.log("未知 fogState " + fogState);
             debugger;
         }
     }
-   
+
 });
