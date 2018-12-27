ed7c821edb4b4a1775e43e434806bd71c8f15ba3
gurry310 <40969925+gurry310@users.noreply.github.com>
2018-12-27 09:15:37 +0800

    Revert "Update DataMgr.js"
    This reverts commit 52caf2389037661eed2682b068f1985891f2735f.

--- assets/Script/DataMgr.js
+++ assets/Script/DataMgr.js
@@ -15,7 +15,7 @@ const {
 @ccclass
 export default class DataMgr extends cc.Component {
     //以年月日 时分 来标记版本，目前只用于清空数据
-    version = "2018-12-25-1766"
+    version = "2018-12-25-1755"
 
     //是否有地图数据，没有就从界面读取，有就从用户数据读取
     hasTileData = false;
@@ -96,47 +96,47 @@ export default class DataMgr extends cc.Component {
             "flowerLevel": 2,
             "minDragonLevel": 1,
             "heartLevel": 0,
-            "needTime": 8
+            "needTime": 1
         },
 
         {
             "flowerLevel": 3,
-            "minDragonLevel": 1,
-            "heartLevel": 0,
-            "needTime": 4
+            "minDragonLevel": 2,
+            "heartLevel": 1,
+            "needTime": 5
         },
 
         {
             "flowerLevel": 4,
-            "minDragonLevel": 1,
-            "heartLevel": 1,
-            "needTime": 8
+            "minDragonLevel": 3,
+            "heartLevel": 2,
+            "needTime": 5
         },
 
         {
             "flowerLevel": 5,
-            "minDragonLevel": 1,
-            "heartLevel": 1,
-            "needTime": 4
+            "minDragonLevel": 4,
+            "heartLevel": 3,
+            "needTime": 5
         }
     ];
     //每级龙的初始体力值
     dragonStrengthDatas = [
         {
             "dragonLevel": 1,
-            "dragonStrength": 3
+            "dragonStrength": 2
         },
         {
             "dragonLevel": 2,
-            "dragonStrength": 11
+            "dragonStrength": 5
         },
         {
             "dragonLevel": 3,
-            "dragonStrength": 36
+            "dragonStrength": 1
         },
         {
             "dragonLevel": 4,
-            "dragonStrength": 112
+            "dragonStrength": 5
         }
     ];
     //每级心的力量
@@ -155,11 +155,11 @@ export default class DataMgr extends cc.Component {
         },
         {
             "heartLevel": 3,
-            "heartStrength": 41
+            "heartStrength": 40
         },
         {
             "heartLevel": 4,
-            "heartStrength": 126
+            "heartStrength": 121
         }
     ];
 
@@ -174,7 +174,7 @@ export default class DataMgr extends cc.Component {
         //龙蛋
         {
             "name":"dragonEgg",
-            "price": 8
+            "price": 1
         },
         
        
@@ -304,21 +304,14 @@ export default class DataMgr extends cc.Component {
         {
             "category": "coin",
             "count": 1,
-            "probability": 0.3
+            "probability": 0.2
         },
 
         {
             "category": "flower",
             "level": 1,
-            "probability": 0.8
+            "probability": 0.4
         },
-
-        // {
-        //     "category": "flower",
-        //     "level": 2,
-        //     "probability": 1.0
-        // },
-
 
         {
             "category": "dragon",
@@ -337,23 +330,23 @@ export default class DataMgr extends cc.Component {
     //duration 单位：秒 休息时间
     dragonNestDuration = [
         {
-            "dragonLe2vel": 1,
-            "duration": 60,
+            "dragonLevel": 1,
+            "duration": 10,
         },
 
         {
             "dragonLevel": 2,
-            "duration": 60
+            "duration": 4
         },
 
         {
             "dragonLevel": 3,
-            "duration": 60
+            "duration": 4
         },
 
         {
             "dragonLevel": 4,
-            "duration": 60
+            "duration": 4
         },
     ];
 
