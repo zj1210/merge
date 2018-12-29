e50c331c12a66da0ce44435ab80f34e1b358a9b6
dibashi <dibashi1989@163.com>
2018-12-29 14:52:22 +0800

    修复云被解锁 多次点击 扣多次钱的bug

--- assets/Script/Fog.js
+++ assets/Script/Fog.js
@@ -152,6 +152,7 @@ cc.Class({
 
         var heartCount = cc.dataMgr.getHeartCount();
         if (heartCount >= this.fogAmount) {
+            this.fog.getComponent(cc.Button).interactable = false;
             //心够，将来可能是弹窗
             //解锁雾 可以在动画回调里设置
             cc.dataMgr.addHeartCount(-this.fogAmount);
