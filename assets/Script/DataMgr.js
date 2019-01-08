5684459af1a627fca6b87312d1a4a961dc2fbbf2
dibashi <dibashi1989@163.com>
2019-01-08 17:24:29 +0800

    取消注释

--- assets/Script/DataMgr.js
+++ assets/Script/DataMgr.js
@@ -504,7 +504,7 @@ export default class DataMgr extends cc.Component {
     init() {
         cc.game.on(cc.game.EVENT_HIDE, function () {
             console.log("datamgr  hide");
-           // cc.dataMgr.saveGameData();
+           cc.dataMgr.saveGameData();
         });
         cc.game.on(cc.game.EVENT_SHOW, function () {
             console.log("datamgr  show");
